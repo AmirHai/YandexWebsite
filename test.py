a9d9e8062a3d59e32155baaa76e75a051896040a
Amir <amirrf06@gmail.com>
2022-03-29 21:17:14 +0300

    create base.html and register.html

---
+++ test.py
@@ -0,0 +1,27 @@
+# Код не работает
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import EmailField, PasswordField, StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    about = TextAreaField("Немного о себе")
+    submit = SubmitField('Войти')
+
+
+app = Flask(__name__)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    # return render_template('register.html', title="Регистрация", form=form)
+
+
+if __name__ == '__main__':
+    app.run()
