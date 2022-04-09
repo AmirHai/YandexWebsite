3d141ccbc4ab5adc287df69810e52febd119b802
Amir <amirrf06@gmail.com>
2022-04-09 12:38:21 +0300

    update html

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-# Код не работает
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField, StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
+from flask_login import current_user
+
+from data import db_session
 
 
 class RegisterForm(FlaskForm):
@@ -21,8 +23,10 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
-    return render_template('register.html', title="Регистрация", form=form)
+    return render_template('register.html', title="Регистрация", form=form,
+                           current_user=current_user)
 
 
 if __name__ == '__main__':
+    db_session.global_init('db/forum.db')
     app.run()
