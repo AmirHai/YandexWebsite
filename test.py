46e5b1f29a3807dc59439a38e9d652ebc71160cc
Ayaz <voridanaya@gmail.com>
2022-04-16 12:22:45 +0300

    register

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField, StringField, SubmitField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired
 from flask_login import current_user
-
+from data.user import User
 from data import db_session
 
 
@@ -28,10 +28,28 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def register():
+def reqister():
     form = RegisterForm()
-    return render_template('register.html', title="Регистрация", form=form,
-                           current_user=current_user)
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form, current_user=current_user)
 
 
 @app.route('/login', methods=['GET', 'POST'])
